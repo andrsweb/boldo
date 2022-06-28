aa4c475dc0068dcba4932c0a995d17a690521815
proflinervk <proflinervk@gmail.com>
2022-06-28 22:16:18 +0300

    some responsive

--- app/js/sections/connect.js
+++ app/js/sections/connect.js
@@ -1,21 +1,4 @@
 document.addEventListener( 'DOMContentLoaded', () => { 
 	'use strict' 
     
-    itemWrapp()
 } )
-
-const itemWrapp = () => {
-
-    const connectItemWrapp = document.querySelectorAll('.connect-item')
-    if ( ! connectItemWrapp.length) return
-    console.log(connectItemWrapp.length)
-
-    connectItemWrapp.forEach( itemWrapp => {
-        itemWrapp.addEventListener('click', () => {
-            if ( ! itemWrapp.classList.contains('active'))
-                itemWrapp.classList.add('active')
-            else
-                itemWrapp.classList.remove('active')
-        })
-    })
-}
